b1a631518a4ee9fed65c38e3b9d1c5fdd6e9459f
amitrathik <amit.rathi.k@gmail.com>
2017-09-09 15:17:26 -0400

    adding routing and filtering

---
+++ src/components/router/Router.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react'
+
+const getCurrentPath = () => {
+  const path = document.location.pathname
+  return path.substring(path.lastIndexOf('/'))
+}
+
+export class Router extends Component{
+  state = {
+    route: getCurrentPath()
+  }
+
+  static childContextTypes = {
+      route : React.PropTypes.string,
+      linkHandler: React.PropTypes.func
+  }
+
+  getChildContext() {
+    return {
+      route : this.state.route,
+      linkHandler : this.handleLinkClick
+    }
+  }
+  handleLinkClick = (route) => {
+    this.setState({route})
+    window.history.pushState(null,'',route)
+  }
+  render() {
+    return <div>{this.props.children}</div>
+  }
+}
--- src/lib/todoHelpers.js
+++ src/lib/todoHelpers.js
@@ -17,3 +17,14 @@ export const removeTodo = (list,id) => {
     ...list.slice(removedIndex+1)
   ]
 }
+export const filterTodos = (list,route) => {
+  switch(route){
+    case '/active':
+      return list.filter(item => !item.isComplete)
+    case '/complete':
+      return list.filter(item => item.isComplete)
+    default:
+      return list
+  }
+
+}
